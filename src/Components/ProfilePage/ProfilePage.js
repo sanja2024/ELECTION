15737e2bf4961d9f374cdbee4b375cec8374b583
pranjalnigam1 <pranjalnigam1234@gmail.com>
2024-02-19 22:10:38 +0530

    profile page done

--- src/Components/ProfilePage/ProfilePage.js
+++ src/Components/ProfilePage/ProfilePage.js
@@ -3,6 +3,16 @@ import Header from '../Header/Header'
 import Footer from '../Footer/Footer'
 import logo from "../../Common/asset/images/voteimg/admin_icon.svg";
 import "./ProfilePage.css"
+
+import userprofile from "../../Common/asset/images/voteimg/Group 17.svg"
+import phone from "../../Common/asset/images/voteimg/phone.svg"
+import vid from "../../Common/asset/images/voteimg/voterid_no.svg"
+import email from "../../Common/asset/images/voteimg/email_icon.svg"
+import profilelogo from "../../Common/asset/images/voteimg/profilelogo.svg"
+
+
+
+
 const ProfilePage = () => {
     return (
         <div>
@@ -17,8 +27,42 @@ const ProfilePage = () => {
                     </div>
                 </div>
 
-                <div className="welcomebackCS pt-5">
-                    
+                <div className="welcomebackprofile pt-5 p-3">
+                <div className="prof_logo">
+          <img className='proflogo' src={profilelogo}></img>
+        </div>
+                    <div className='profile_info'>
+                        <div>
+                            <img src={userprofile}></img>
+                        </div>
+                        <div className='profilename'>
+                            <span >U Namasaivayam Vadivelan</span>
+                        </div>
+                    </div>
+                    <div className='profile_info'>
+                        <div>
+                            <img src={phone}></img>
+                        </div>
+                        <div className='profilename'>
+                            <span >+91 9856743678</span>
+                        </div>
+                    </div>
+                    <div className='profile_info'>
+                        <div>
+                            <img src={vid}></img>
+                        </div>
+                        <div className='profilename'>
+                            <span >UTC654237</span>
+                        </div>
+                    </div>
+                    <div className='profile_info'>
+                        <div>
+                            <img src={email}></img>
+                        </div>
+                        <div className='profilename'>
+                            <span >namasivayam@gmail.com</span>
+                        </div>
+                    </div>
 
 
 
